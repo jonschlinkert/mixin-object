76fdf389532a1dc1b503549f5ae3c26327cbf729
jonschlinkert <jon.schlinkert@sellside.com>
2015-08-21 02:24:00 -0400

    fixes sparse arguments bug

--- index.js
+++ index.js
@@ -3,16 +3,10 @@
 var isObject = require('is-extendable');
 var forIn = require('for-in');
 
-module.exports = function mixin(target, objects) {
+function mixin(target, objects) {
   if (!isObject(target)) {
     throw new TypeError('mixin-object expects the first argument to be an object.');
   }
-
-  // if no additional objects are defined, return the first one.
-  if (!isObject(objects)) {
-    return target;
-  }
-
   var len = arguments.length, i = 0;
   while (++i < len) {
     var obj = arguments[i];
@@ -21,8 +15,22 @@ module.exports = function mixin(target, objects) {
     }
   }
   return target;
-};
+}
+
+/**
+ * copy properties from the source object to the
+ * target object.
+ *
+ * @param  {*} `value`
+ * @param  {String} `key`
+ */
 
 function copy(value, key) {
   this[key] = value;
 }
+
+/**
+ * Expose `mixin`
+ */
+
+module.exports = mixin;
