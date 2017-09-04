3d64ae9e2319fd2e7c48a861f7d245e24dcba72d
jonschlinkert <jon.schlinkert@sellside.com>
2017-09-04 05:53:14 -0400

    use assert

--- test.js
+++ test.js
@@ -1,45 +1,47 @@
 'use strict';
 
-/* deps: mocha */
-require('should');
+require('mocha');
+var assert = require('assert');
 var mixin = require('./');
 
-describe('.mixin()', function () {
-  it('should throw an error when the first argument is not an object.', function () {
-    (function () {
+describe('mixin-object', function() {
+  it('should throw an error when the first argument is not an object.', function() {
+    assert.throws(function() {
       mixin();
-    }).should.throw('mixin-object expects the first argument to be an object.');
+    });
   });
 
-  it('should mix all objects into the first one.', function () {
+  it('should mix all objects into the first one.', function() {
     var obj = {c: 'c'};
     mixin(obj, {a: 'a'}, {b: 'b'});
-    obj.should.eql({c: 'c', a: 'a', b: 'b'});
+    assert.deepEqual(obj, {c: 'c', a: 'a', b: 'b'});
   });
 
-  it('should return a new object when an empty object is passed.', function () {
+  it('should return a new object when an empty object is passed.', function() {
     var c = {c: 'c'};
     var d = mixin({}, c, {a: 'a'}, {b: 'b'});
-    c.should.eql({c: 'c'});
-    d.should.eql({c: 'c', a: 'a', b: 'b'});
+    assert.deepEqual(c, {c: 'c'});
+    assert.deepEqual(d, {c: 'c', a: 'a', b: 'b'});
   });
 
-  it('should mix properties onto a function.', function () {
+  it('should mix properties onto a function.', function() {
     function foo() {}
     var c = {c: 'c'};
     var d = mixin(foo, c, {a: 'a'}, {b: 'b'});
-    foo.should.have.properties(['a', 'b', 'c']);
+    assert(foo.hasOwnProperty('a'));
+    assert(foo.hasOwnProperty('b'));
+    assert(foo.hasOwnProperty('c'));
   });
 
-  it('should mixin all objects.', function () {
-    mixin({a: 'a'}, {b: 'b'}).should.eql({a: 'a', b: 'b'});
-    mixin({a: 'a'}, {b: 'b'}, {c: 'c'}).should.eql({a: 'a', b: 'b', c: 'c'});
-    mixin({a: 'a'}, {b: {c: 'c'}}, {d: 'd'}).should.eql({a: 'a', b: {c: 'c'}, d: 'd'});
+  it('should mixin all objects.', function() {
+    assert.deepEqual(mixin({a: 'a'}, {b: 'b'}), {a: 'a', b: 'b'});
+    assert.deepEqual(mixin({a: 'a'}, {b: 'b'}, {c: 'c'}), {a: 'a', b: 'b', c: 'c'});
+    assert.deepEqual(mixin({a: 'a'}, {b: {c: 'c'}}, {d: 'd'}), {a: 'a', b: {c: 'c'}, d: 'd'});
   });
 
-  it('should create a new object.', function () {
-    mixin({}, {a: 'a'}, {b: 'b'}).should.eql({a: 'a', b: 'b'});
-    mixin({}, {a: 'a'}, {b: 'b'}, {c: 'c'}).should.eql({a: 'a', b: 'b', c: 'c'});
-    mixin({}, {a: 'a'}, {b: {c: 'c'}}, {d: 'd'}).should.eql({a: 'a', b: {c: 'c'}, d: 'd'});
+  it('should create a new object.', function() {
+    assert.deepEqual(mixin({}, {a: 'a'}, {b: 'b'}), {a: 'a', b: 'b'});
+    assert.deepEqual(mixin({}, {a: 'a'}, {b: 'b'}, {c: 'c'}), {a: 'a', b: 'b', c: 'c'});
+    assert.deepEqual(mixin({}, {a: 'a'}, {b: {c: 'c'}}, {d: 'd'}), {a: 'a', b: {c: 'c'}, d: 'd'});
   });
 });
